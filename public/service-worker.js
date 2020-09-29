f3d47b3d36fb9528f7240d1afe282bcf0ce926c3
AtimaB <nickzehn10@hotmail.com>
2020-09-28 19:38:51 -0600

    offline is working

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,14 +1,14 @@
 const FILES_TO_CACHE = [
   "/",
-  "/index.html",
-  "/style.css",
-  "/manifest.webmanifest.json",
+  "/index.js",
+  "/styles.css",
+  "/manifest.webmanifest",
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png",
 ];
 
-const CACHE_NAME = "static-cache-v2";
-const DATA_CACHE_NAME = "data-cache-v1";
+const CACHE_NAME = "static-cache-bd2";
+const DATA_CACHE_NAME = "data-cache-bd1";
 
 // install
 self.addEventListener("install", function (evt) {
