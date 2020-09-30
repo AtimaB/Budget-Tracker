b233984bd0400f8f2402bf295090ca6d4357681d
AtimaB <nickzehn10@hotmail.com>
2020-09-30 17:02:05 -0600

    edit

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,12 +3,12 @@ const DATA_CACHE_NAME = "data-cache-v1";
 
 const FILES_TO_CACHE = [
   "/",
-  "/db.js",
   "/index.js",
   "/manifest.webmanifest",
   "/styles.css",
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png",
+  "/db.js",
 ];
 
 // install
