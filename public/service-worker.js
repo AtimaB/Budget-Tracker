796bf4c9bd5f1d630ba2c0d2bdb08f87cb8190de
AtimaB <nickzehn10@hotmail.com>
2020-09-30 19:09:36 -0600

    edit

--- public/service-worker.js
+++ public/service-worker.js
@@ -16,13 +16,14 @@ self.addEventListener("install", function (evt) {
   evt.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       console.log("Your files were pre-cached successfully!");
-      return cache.addAll(FILES_TO_CACHE);
+      return cache.addAll(staticFilesToPreCache);
     })
   );
 
   self.skipWaiting();
 });
 
+// activate
 self.addEventListener("activate", function (evt) {
   evt.waitUntil(
     caches.keys().then((keyList) => {
@@ -42,8 +43,8 @@ self.addEventListener("activate", function (evt) {
 
 // fetch
 self.addEventListener("fetch", function (evt) {
-  // cache successful requests to the API
-  if (evt.request.url.includes("/api/")) {
+  const { url } = evt.request;
+  if (url.includes("/api/")) {
     evt.respondWith(
       caches
         .open(DATA_CACHE_NAME)
@@ -52,7 +53,7 @@ self.addEventListener("fetch", function (evt) {
             .then((response) => {
               // If the response was good, clone it and store it in the cache.
               if (response.status === 200) {
-                cache.put(evt.request.url, response.clone());
+                cache.put(evt.request, response.clone());
               }
 
               return response;
@@ -64,15 +65,14 @@ self.addEventListener("fetch", function (evt) {
         })
         .catch((err) => console.log(err))
     );
-
-    return;
+  } else {
+    // respond from static cache, request is not for /api/*
+    evt.respondWith(
+      caches.open(CACHE_NAME).then((cache) => {
+        return cache.match(evt.request).then((response) => {
+          return response || fetch(evt.request);
+        });
+      })
+    );
   }
-
-  // if the request is not for the API, serve static assets using "offline-first" approach.
-  // see https://developers.google.com/web/fundamentals/instant-and-offline/offline-cookbook#cache-falling-back-to-network
-  evt.respondWith(
-    caches.match(evt.request).then(function (response) {
-      return response || fetch(evt.request);
-    })
-  );
 });
